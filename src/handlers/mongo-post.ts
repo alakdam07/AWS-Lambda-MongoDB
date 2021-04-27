f3e5b0aa8c34d8dde34ec7284bcae0ef98dc8790
alakdam07 <smashalak2003@yahoo.com>
2021-04-27 07:18:37 +0300

    mongo post request added

---
+++ src/handlers/mongo-post.ts
@@ -0,0 +1,39 @@
+import { Response } from '../utils/lambda-response'
+import runWarm from '../utils/run-warm'
+import connect from '../services/mongodb/connect';
+
+export interface ITodo {
+  todo: string;
+  status: boolean;
+}
+/**
+ * Mongo-Handler
+ */
+export async function mongoPost(event: AWSLambda.APIGatewayEvent): Promise<Response> {
+
+  const client = await connect('editor');
+  const requestBody = JSON.parse(event.body || '{}');
+
+  const Item: ITodo = {
+    todo: requestBody.todo,
+    status: false
+  }
+
+  await client
+    .db('Todo')
+    .collection('todo').insertOne(Item)
+
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(requestBody),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
+}
+
+export default runWarm(mongoPost)
